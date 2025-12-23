b237c3a93ac4e41c7b2652adcc5e6ee3d836ed6b
Krishna Waske <krishna.waske@ayanworks.com>
2025-12-23 17:53:38 +0530

    fix: x509 controller (#338)
    * fix: x509 controller
    
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>
    
    * fix: x509 import fix
    
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>
    
    * fix/added dynamic implementation for keyType of x509
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * fix/sonarqube issue
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * fix/sonarqube issue
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * fix/sonar cube issue
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * fix/code rabbit comments
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * fix/pr comments
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    ---------
    
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    Co-authored-by: sujitaw <sujit.sutar@ayanworks.com>

--- src/controllers/x509/x509.types.ts
+++ src/controllers/x509/x509.types.ts
@@ -1,7 +1,9 @@
 import type { X509ExtendedKeyUsage, X509KeyUsage } from '@credo-ts/core'
-import { KeyAlgorithm } from '@openwallet-foundation/askar-nodejs'
+import type { KeyAlgorithm } from '@openwallet-foundation/askar-nodejs'
 
 import { Extension, Example } from 'tsoa'
+
+import { DidMethod } from '../../enums/enum'
 
 // Enum remains the same
 export enum GeneralNameType {
@@ -33,7 +35,7 @@ export interface AuthorityAndSubjectKeyDto {
    * @description Type of the key used for signing the X.509 Certificate (default is p256)
    */
   // FIXME: Check type
-  keyType?: KeyAlgorithm
+  keyType?: Curve
 }
 
 export interface NameDto {
@@ -203,4 +205,62 @@ export interface X509CreateCertificateOptionsDto {
 
   validity?: ValidityDto
   extensions?: X509CertificateExtensionsOptionsDto
+}
+
+export type OkpCurve = 'Ed25519' | 'X25519'
+export type EcCurve = 'P-256' | 'P-384' | 'P-521' | 'secp256k1'
+
+export type OkpType = {
+  kty: 'OKP'
+  crv: 'Ed25519' | 'X25519'
+}
+
+export type EcType = {
+  kty: 'EC'
+  crv: 'P-256' | 'P-384' | 'P-521' | 'secp256k1'
+}
+
+export type Curve = 'Ed25519' | 'X25519' | 'P-256' | 'P-384' | 'P-521' | 'secp256k1'
+
+export enum RsaModulusLength {
+  RSA_2048 = 2048,
+  RSA_3072 = 3072,
+  RSA_4096 = 4096,
+}
+
+export type KmsCreateKeyTypeAssymetric =
+  | {
+      kty: 'OKP'
+      crv: 'Ed25519' | 'X25519'
+    }
+  | {
+      kty: 'EC'
+      crv: 'P-256' | 'P-384' | 'P-521' | 'secp256k1'
+    }
+
+export const supportedKeyTypesDID: Record<DidMethod, readonly { kty: string; crv: string }[]> = {
+  [DidMethod.Indy]: [{ kty: 'OKP', crv: 'Ed25519' }],
+
+  [DidMethod.Peer]: [
+    { kty: 'OKP', crv: 'Ed25519' },
+    { kty: 'OKP', crv: 'X25519' },
+  ],
+
+  [DidMethod.Key]: [
+    { kty: 'OKP', crv: 'Ed25519' },
+    { kty: 'OKP', crv: 'X25519' },
+    { kty: 'EC', crv: 'P-256' },
+    { kty: 'EC', crv: 'P-384' },
+    { kty: 'EC', crv: 'P-521' },
+    { kty: 'EC', crv: 'secp256k1' },
+  ],
+
+  [DidMethod.Web]: [
+    { kty: 'OKP', crv: 'Ed25519' },
+    { kty: 'OKP', crv: 'X25519' },
+    { kty: 'EC', crv: 'P-256' },
+    { kty: 'EC', crv: 'secp256k1' },
+  ],
+
+  [DidMethod.Polygon]: [{ kty: 'EC', crv: 'secp256k1' }],
 }
